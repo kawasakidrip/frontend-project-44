ca83e96dcdf03672b36f7102bf8312a305f02804
Vlad <khmara.vladislav@hexly.ru>
2022-11-30 13:37:49 +0300

    added js packets

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import inputName from '../src/cli.js';
+inputName();
+
+
---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+const inputName = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name?: ');
+    console.log(`Hello, ${name}!`);
+}
+export default inputName;
