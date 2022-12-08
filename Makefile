752eab1d943189f891e5e1d008281b2bb1f3c48f
kawasakidrip <118714420+kawasakidrip@users.noreply.github.com>
2022-12-08 21:33:46 +0300

    prime

--- Makefile
+++ Makefile
@@ -16,3 +16,5 @@ brain-gcd:
 		node bin/brain-gcd.js
 brain-progression:
 		node bin/brain-progression.js
+brain-prime:
+		node bin/brain-prime.js
---
+++ bin/brain-prime.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+
+import { getRandom } from '../src/function.js';
+import logics from '../src/index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const primeCheck = (number) => {
+    if (number < 2) {
+        return false;
+    }
+    for (let i = 2; i < number; i += 1) {
+        if (number % i) {
+        return false;
+    };
+}
+return true
+};
+
+const game = () => {
+  const question = getRandom(1, 100);
+  const answer = primeCheck(question) ? 'yes' : 'no';
+
+  return [question, answer];
+};
+
+const start = () => logics(description, game);
+start();
+export default start;
