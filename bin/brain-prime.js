5c8a8ce91d40135ea3e595842a90b0229eebdb22
kawasakidrip <118714420+kawasakidrip@users.noreply.github.com>
2022-12-08 22:05:40 +0300

    finaly

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -8,15 +8,15 @@ import logics from '../src/index.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const primeCheck = (number) => {
-    if (number < 2) {
-        return false;
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i < number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
     }
-    for (let i = 3; i < number / 2; i += 1) {
-        if (number % i === 0) {
-        return false;
-    }
-}
-return true
+  }
+  return true;
 };
 
 const game = () => {
