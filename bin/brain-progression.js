1bd477d8e16342eb0d629be8494a3810a5ccca29
kawasakidrip <leinchmo@gmail.com>
2022-12-08 20:40:16 +0300

    progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -5,28 +5,28 @@
 import { getRandom } from '../src/function.js';
 import logics from '../src/index.js';
 
-const description = 'What number is missing in the progression?'
+const description = 'What number is missing in the progression?';
 
 const getProgression = (number, length, skip) => {
-    let progression = [];
-    for (let i = 0; i < 10; i += 1) {
-        progression.push(number + i * skip);
-    }
-    return progression
-  };
-
-  const game = () => {
-    const length = 10;
-    const number = getRandom(1, 100);
-    const quest = getRandom(1, 10);
-    const skip = getRandom(1, 5);
-    const progression = getProgression(number, length, skip);
-    progression[quest] = '..';
-    const question = progression;
-    const answer = (number + quest * skip).toString()
-    
-    return [question, answer];
+  const progression = [];
+  for (let i = 0; i < 10; i += 1) {
+    progression.push(number + i * skip);
   }
+  return progression;
+};
+
+const game = () => {
+  const length = 10;
+  const number = getRandom(1, 100);
+  const quest = getRandom(1, 10);
+  const skip = getRandom(1, 5);
+  const progression = getProgression(number, length, skip);
+  progression[quest] = '..';
+  const question = progression.join(' ');
+  const answer = (number + quest * skip).toString();
+
+  return [question, answer];
+};
 
 const start = () => logics(description, game);
 start();
